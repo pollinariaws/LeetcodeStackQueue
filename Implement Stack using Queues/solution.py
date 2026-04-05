51ddc03ff070bf94f5421d1949fd0ea9e2c4a629
pollinariaws <polinazuzuk@gmail.com>
2026-04-05 20:46:27 +0300

    Update solution.py
    redo it using linked list

--- Implement Stack using Queues/solution.py
+++ Implement Stack using Queues/solution.py
@@ -1,29 +1,45 @@
-from collections import deque
+class ListNode:
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
 class Queue:
     def __init__(self):
-        self.data = deque()
-    def push(self, x):
-        self.data.append(x)
-    def pop(self):
-        return self.data.popleft()
-    def peek(self):
-        return self.data[0]
-    def empty(self):
-        return len(self.data) == 0
-    def size(self):
-        return len(self.data)
+        self.head = None
+        self.tail = None
+        self.size = 0
+    def push(self, x: int) -> None:
+        node = ListNode(x)
+        if self.tail:
+            self.tail.next = node
+        else:
+            self.head = node
+        self.tail = node
+        self.size += 1
+    def pop(self) -> int:
+        val = self.head.val
+        self.head = self.head.next
+        if self.head is None:
+            self.tail = None
+        self.size -= 1
+        return val
+    def peek(self) -> int:
+        return self.head.val
+    def empty(self) -> bool:
+        return self.head is None
+    def size(self) -> int:
+        return self.size
 class MyStack:
     def __init__(self):
         self.q1 = Queue()
         self.q2 = Queue()
-    def push(self, x):
+    def push(self, x: int) -> None:
         self.q2.push(x)
         while not self.q1.empty():
             self.q2.push(self.q1.pop())
         self.q1, self.q2 = self.q2, self.q1
-    def top(self):
-        return self.q1.peek()
-    def empty(self):
-        return self.q1.empty()
-    def pop(self):
+    def pop(self) -> int:
         return self.q1.pop()
+    def top(self) -> int:
+        return self.q1.peek()
+    def empty(self) -> bool:
+        return self.q1.empty()
